b4c85b3ef911c81ce82af6ef7dd21f075beb0854
portones00-svg <portones00@gmail.com>
2026-05-28 21:50:18 -0400

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+const CACHE='mr-v1';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.url.includes('/api/')){
+    e.respondWith(fetch(e.request));return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
